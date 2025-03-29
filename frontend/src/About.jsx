78af0fc8c60afe89ad1a56d2ee5b72e049f07aa0
TSDPRASAD88 <prasad.thamarana2006@gmail.com>
2025-03-29 10:59:26 +0530

    react-frontend

--- frontend/src/About.jsx
+++ frontend/src/About.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const About = () => {
   return (
     <div>
-      About
+        <h1>Project Title: Salon Horror: The Worst Haircut Disasters </h1>
+      <p>This project showcases a collection of the worst haircut disasters seen at salons, featuring images and descriptions of styling mishaps. Users can browse through real-life examples of bad haircuts, learn what went wrong, and even share their own experiences.</p>
     </div>
   )
 }
